677247a3894e2151097e6428cbb38831cd13c653
Frederic Lauzier <lauzier.frederic@gmail.com>
2020-11-09 12:45:56 -0500

    Stupid coach running correctly for all scenarios given, no frontend yet

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,5 +4,15 @@ class QuestionsController < ApplicationController
   end
 
   def answer
+    @question = params[:question]
+    standardresp = 'get dressed and go to work'
+
+    if @question.downcase == 'i am going to work'
+      @answer = 'Great!'
+    elsif @question.include? '?'
+      @answer = "Silly question, #{standardresp}"
+    else
+      @answer = "I don't care, #{standardresp}"
+    end
   end
 end
